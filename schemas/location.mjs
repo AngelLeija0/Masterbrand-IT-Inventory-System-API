b2495c64a28bfc28a1647c15fb93ddfca570e1c7
Angel Gael Leija Limones <angel.leija@uttn.mx>
2023-11-04 09:26:49 -0500

    Correction in date format

--- schemas/location.mjs
+++ schemas/location.mjs
@@ -7,8 +7,12 @@ const locationSchema = z.object({
 })
 
 function validateLocation (object) {
-  object.created_at = new Date(object.created_at)
-  object.updated_at = new Date(object.updated_at)
+  if (object.created_at) {
+    object.created_at = new Date(object.created_at)
+  }
+  if (object.updated_at) {
+    object.updated_at = new Date(object.updated_at)
+  }
   return locationSchema.safeParse(object)
 }
 
