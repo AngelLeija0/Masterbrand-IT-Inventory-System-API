3f01237904c3394906f44619200dcc4cae5445e7
Angel Gael Leija Limones <angel.leija@uttn.mx>
2023-11-05 07:01:39 -0600

    Corrections

--- routes/locations/delete.mjs
+++ routes/locations/delete.mjs
@@ -5,7 +5,7 @@ import Location from '../../models/location.mjs'
 
 router.delete('/delete/:id', async (req, res) => {
     await Location.findByIdAndDelete(req.params.id)
-    res.status(200)
+    res.status(200).json({ message: "Delete it correctly" })
   try {
   } catch (error) {
     res.status(500).json({ message: error.message })
