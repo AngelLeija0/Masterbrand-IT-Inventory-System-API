2c7ebff8a25a67cad5c519cc7bf2948758ceb7c4
Angel Gael Leija Limones <Angel.Leija@masterbrand.com>
2024-01-11 11:57:30 -0600

    Corrections in asset schema

--- schemas/asset.mjs
+++ schemas/asset.mjs
@@ -47,7 +47,7 @@ const assetSchema = z.object({
   operating_system: z.string().optional(),
   ip_address: z.string().optional(),
   ram: z.string().optional(),
-  network_status: z.string().optional(),
+  network_status: z.optional(),
   quantity: z.string().optional(),
   quantity_last_out: z.string().optional(),
   date_quantity_last_out: z.string().optional(),
